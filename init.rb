5f7075718d4fc040ee58c88a3f6a8a9d8ae00526
Irina Lebedevich <irina.lebedevich@undev.ru>
2015-01-16 13:43:35 +0300

    ref #56825 Init.rb updated

--- init.rb
+++ init.rb
@@ -5,9 +5,9 @@ Rails.application.paths["app/overrides"] ||= []
 Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
 
 Redmine::Plugin.register :redmine_user_specific_theme do
-  name        'Redmine user specific theme plugin'
-  description 'Theme setting at the user account'
-  author      'Danil Tashkinov'
+  name        'Redmine User-Specific Theme Plugin'
+  description 'This plugin allows users to set Redmine themes in the account settings.'
+  author      'Undev'
   version     '0.0.1'
   url         'https://github.com/Undev/redmine_user_specific_theme'
 
