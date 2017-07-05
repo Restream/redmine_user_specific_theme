82d60351b15b0df8509326daeb33f5733b53491f
t-gergely <t-gergely@users.noreply.github.com>
2017-07-05 16:15:57 +0200

    safe_attributes for Redmine 3.4+

--- lib/redmine_user_specific_theme/patches/user_preference_patch.rb
+++ lib/redmine_user_specific_theme/patches/user_preference_patch.rb
@@ -4,6 +4,14 @@ module RedmineUserSpecificTheme
   module Patches
     module UserPreferencePatch
 
+      def self.prepended(base)
+        base.class_eval do
+          if defined? safe_attributes
+            safe_attributes :ui_theme
+          end
+        end
+      end
+
       def ui_theme
         self[:ui_theme]
       end
