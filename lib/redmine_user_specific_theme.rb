245942af4d0951176143940840fe8caec9692257
Daniel Kallner <daniel@octopuscs.com>
2019-12-29 09:31:05 +0200

    Add support for Redmine 4

--- lib/redmine_user_specific_theme.rb
+++ lib/redmine_user_specific_theme.rb
@@ -5,7 +5,7 @@ require_dependency 'redmine_user_specific_theme/hooks/view_hooks'
 require_dependency 'redmine_user_specific_theme/patches/user_preference_patch'
 require_dependency 'redmine_user_specific_theme/patches/application_helper_patch'
 
-ActionDispatch::Callbacks.to_prepare do
+ActiveSupport::Reloader.to_prepare do
 
   unless UserPreference.included_modules.include?(RedmineUserSpecificTheme::Patches::UserPreferencePatch)
     UserPreference.send :prepend, RedmineUserSpecificTheme::Patches::UserPreferencePatch
